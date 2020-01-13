b1f9041af0c0009734d9dbe886b9eb2cf8a809ce
pnorton <peter.norton@gazprom-mt.com>
2020-01-13 16:35:42 +0000

    new sort method

--- json_sort.py
+++ json_sort.py
@@ -1,13 +1,20 @@
 import json
 from pandas.io.json import json_normalize
 
-def sort_by_price_ascending(input):
+def sort_by_price_ascending1(input):
     data = json.loads(input)
     df = json_normalize(data)
     df.sort_values(by=['price', 'name'], inplace=True)
     j = df.to_json(orient='records')
     return j
 
-input = '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
-expected_output =  '[{"name":"eggs","price":1},{"name":"rice","price":4.04},{"name":"coffee","price":9.99}]'
-assert sort_by_price_ascending(input) == expected_output
+def sort_by_price_ascending2(input):
+    data = json.loads(input)
+    return sorted(data, key=lambda x: (x['price'], x['name']))
+
+
+input_str = '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
+expected_output_str = '[{"name":"eggs","price":1},{"name":"rice","price":4.04},{"name":"coffee","price":9.99}]'
+expected_output_json = json.loads(expected_output_str)
+
+assert sort_by_price_ascending2(input_str) == expected_output_json
