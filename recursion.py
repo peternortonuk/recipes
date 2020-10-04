8f12aeec6e5a28d029d1bf048d1914d3c9c1ed27
Peter Norton <peter.norton@gazprom-mt.com>
2020-10-04 21:09:12 +0100

    comments only

--- recursion.py
+++ recursion.py
@@ -44,6 +44,9 @@ print('\n=========================================================\n'
 
 """
 https://realpython.com/lessons/fibonacci-and-optimizations/
+
+Fibonacci sequence:
+0  1  1  2  3  5  8  13  21  34  55
 """
 
 
